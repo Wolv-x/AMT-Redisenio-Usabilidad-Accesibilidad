9d6cf02c59be418bca81880e9ad797e8b722e65c
Andrés Ponce <75549856+aPonce2001@users.noreply.github.com>
2023-08-06 20:28:40 -0500

    init: Project structure
    Se agregó la estructura del proyecto de código para poder trabajarlo. Se incluyó:
    - Módulos landing-page, error-pages e infringments

--- desarrollo/src/app/app-routing.module.ts
+++ desarrollo/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './error-pages/pages/page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', loadChildren: () => import('./modules/landing-page/landing-page.module').then(m => m.LandingPageModule) },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
