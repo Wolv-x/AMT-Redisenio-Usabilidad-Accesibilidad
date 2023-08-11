bb46d892992585fa4a54117045ed18a7a9179d14
Andrés Ponce <75549856+aPonce2001@users.noreply.github.com>
2023-08-11 00:28:15 -0500

    fix: Direcciones para cada idioma
    Se aseguró que cada idioma tenga una dirección con su / y así puede mantener el idioma

--- desarrollo/src/app/app-routing.module.ts
+++ desarrollo/src/app/app-routing.module.ts
@@ -1,14 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { PageNotFoundComponent } from './error-pages/pages/page-not-found/page-not-found.component';
 
-const routes: Routes = [
-  { path: '', loadChildren: () => import('./modules/landing-page/landing-page.module').then(m => m.LandingPageModule) },
-  { path: '**', component: PageNotFoundComponent }
+import { PageNotFoundComponent } from './error-pages/pages/page-not-found/page-not-found.component';
+import { ParamConstants } from './shared/constants/params-constants';
+import { LanguageConfig } from 'src/config/language-config';
+import { LanguageGuard } from './shared/guards/language/language.guard';
+
+const appRoutes: Routes = [
+  {
+    path: ':' + ParamConstants.LANGUAGE,
+    loadChildren: () => import('./modules/landing-page/landing-page.module').then(m => m.LandingPageModule),
+    canActivate: [LanguageGuard]
+  },
+  {
+    path: '',
+    redirectTo: LanguageConfig.DEFAULT_LANGUAGE,
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent
+  }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- desarrollo/src/app/app.component.ts
+++ desarrollo/src/app/app.component.ts
@@ -1,17 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 import { TranslateService } from '@ngx-translate/core';
 import { LanguageConfig, getSupportedLanguageCodes } from 'src/config/language-config';
+import { ActivatedRoute } from '@angular/router';
+import { ParamConstants } from './shared/constants/params-constants';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   constructor(
-    public translate: TranslateService
-  ) {
-    translate.addLangs(getSupportedLanguageCodes());
-    translate.setDefaultLang(LanguageConfig.DEFAULT_LANGUAGE);
+    public translate: TranslateService,
+    @Inject(DOCUMENT) private document: Document,
+    private route: ActivatedRoute
+  ) { }
+
+  ngOnInit() {
+    const supportedLanguageCodes = getSupportedLanguageCodes();
+    this.translate.addLangs(supportedLanguageCodes);
+    this.translate.setDefaultLang(LanguageConfig.DEFAULT_LANGUAGE);
+    this.route.params.subscribe((params) => {
+      this.translate.use(params[ParamConstants.LANGUAGE]);
+    });
+    this.document.documentElement.lang = this.translate.currentLang;
   }
 }
