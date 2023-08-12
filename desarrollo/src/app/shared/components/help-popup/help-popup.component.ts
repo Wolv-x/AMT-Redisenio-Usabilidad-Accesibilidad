3869ff7b237805d0a724fc26648afe06e058377f
Andrés Ponce <75549856+aPonce2001@users.noreply.github.com>
2023-08-12 11:06:47 -0500

    feat: Librería de estilo de botones
    Librería de estilos de los botones

---
+++ desarrollo/src/app/shared/components/help-popup/help-popup.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-help-popup',
+  templateUrl: './help-popup.component.html',
+  styleUrls: ['./help-popup.component.less']
+})
+export class HelpPopupComponent {
+
+}
