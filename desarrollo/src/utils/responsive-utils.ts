83d28085670ec0e3c56c6e2483a600efaaa8d1e0
Andrés Ponce <75549856+aPonce2001@users.noreply.github.com>
2023-08-12 01:42:35 -0500

    feat: Librería de estilos
    Se creó la librería de estilos Carlitos con less para reutilizarla.
    Se agregó:
    - colores
    - fuentes
    - mixins para diseño responsive

--- desarrollo/src/utils/responsive-utils.ts
+++ desarrollo/src/utils/responsive-utils.ts
@@ -1,4 +1,4 @@
-export const MAX_WIDTH_MOBILE: number = 1024;
+const MAX_WIDTH_MOBILE: number = 1024;
 
 export function isMobileViewport(): boolean {
     return window.innerWidth < MAX_WIDTH_MOBILE;
