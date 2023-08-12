22503a8c19b263e959170649ec3d03f308ad5541
Andrés Ponce <75549856+aPonce2001@users.noreply.github.com>
2023-08-12 10:23:11 -0500

    chore: Comentarios de cambios
    Se añadió un comentario de dónde también se deben cambiar las variables

--- desarrollo/src/utils/responsive-utils.ts
+++ desarrollo/src/utils/responsive-utils.ts
@@ -1,3 +1,4 @@
+//If you want to change this value, you also should change it in /src/styles/base/mixins.less
 const MAX_WIDTH_MOBILE: number = 1024;
 
 export function isMobileViewport(): boolean {
