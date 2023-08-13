b1e1157ab9d577645226284e2121b7aeeab69433
Andrés Ponce <75549856+aPonce2001@users.noreply.github.com>
2023-08-13 17:16:30 -0500

    feat: Language picker
    Selector de idioma para la accesibilidad

--- desarrollo/src/app/shared/shared.module.ts
+++ desarrollo/src/app/shared/shared.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
 import { CommonModule } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
+import { HttpClientModule } from '@angular/common/http';
+import { LanguagePickerComponent } from './components/language-picker/language-picker.component';
 import { MainNavComponent } from './components/main-nav/main-nav.component';
 
 @NgModule({
   declarations: [],
   declarations: [
+    LanguagePickerComponent,
     MainNavComponent,
   ],
   imports: [
