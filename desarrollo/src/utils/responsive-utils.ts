845989cb25fdd9e6cef4dc1040cbec72ff8797bc
Andrés Ponce <75549856+aPonce2001@users.noreply.github.com>
2023-08-13 18:32:08 -0500

    fix: New breakpoint for mobile viewport

--- desarrollo/src/utils/responsive-utils.ts
+++ desarrollo/src/utils/responsive-utils.ts
@@ -1,5 +1,5 @@
 //If you want to change this value, you also should change it in /src/styles/base/mixins.less
-const MAX_WIDTH_MOBILE: number = 1024;
+const MAX_WIDTH_MOBILE: number = 1090;
 
 export function isMobileViewport(): boolean {
     return window.innerWidth < MAX_WIDTH_MOBILE;
