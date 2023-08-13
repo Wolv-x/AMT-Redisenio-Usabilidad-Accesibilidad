db14aecab9adc4a3161155361b929a8172d95fb6
Andrés Ponce <75549856+aPonce2001@users.noreply.github.com>
2023-08-13 17:15:03 -0500

    feat: Main navigation component
    Componente principal de la navegación

---
+++ desarrollo/src/app/shared/components/main-nav/main-nav.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-nav',
+  templateUrl: './main-nav.component.html',
+  styleUrls: ['./main-nav.component.less']
+})
+export class MainNavComponent {
+  constructor() {
+  }
+}
--- desarrollo/src/app/shared/shared.module.ts
+++ desarrollo/src/app/shared/shared.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MainNavComponent } from './components/main-nav/main-nav.component';
 
 @NgModule({
   declarations: [],
+  declarations: [
+    MainNavComponent,
+  ],
   imports: [
     CommonModule,
     HttpClientModule,
     TranslateModule.forChild(),
+    RouterModule
   ],
   ]
 })
