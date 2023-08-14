45e6ef4c67998b6abce02dc7f6d558154e776e30
Andrés Ponce <75549856+aPonce2001@users.noreply.github.com>
2023-08-13 21:49:29 -0500

    fix: Button styles
    Fixed buttons style

--- desarrollo/src/app/shared/components/button/button.component.ts
+++ desarrollo/src/app/shared/components/button/button.component.ts
@@ -2,9 +2,11 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-button',
-  templateUrl: './button.component.html'
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.less']
 })
 export class ButtonComponent {
   @Input() label: string = '';
   @Input() type: string = '';
+  @Input() ariaLabel: string = '';
 }
