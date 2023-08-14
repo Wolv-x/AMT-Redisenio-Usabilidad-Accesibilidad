efd2d4c4e5a24a26c2b71e9d2ba0fa21e69be9f6
Andrés Ponce <75549856+aPonce2001@users.noreply.github.com>
2023-08-14 01:48:46 -0500

    init: Módulo de Landing page iniciado

--- desarrollo/src/app/modules/landing-page/landing-page-routing.module.ts
+++ desarrollo/src/app/modules/landing-page/landing-page-routing.module.ts
@@ -1,12 +1,26 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { PageNotFoundComponent } from 'src/app/error-pages/pages/page-not-found/page-not-found.component';
 
-
+const routes: Routes = [
+  {
+    path: '',
+    component: LandingPageComponent
+  },
+  {
+    path: 'home',
+    component: LandingPageComponent
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent
+  }
+];
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
 })
 export class LandingPageRoutingModule { }
--- desarrollo/src/app/modules/landing-page/landing-page.module.ts
+++ desarrollo/src/app/modules/landing-page/landing-page.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { LandingPageRoutingModule } from './landing-page-routing.module';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LandingPageComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    LandingPageRoutingModule,
+    SharedModule,
+    TranslateModule.forChild()
   ]
 })
 export class LandingPageModule { }
