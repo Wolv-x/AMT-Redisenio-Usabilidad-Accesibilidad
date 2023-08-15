dd389c38db52702692e8d3ef32db582f4a05eccc
Andrés Ponce <andres.ponce@epn.edu.ec>
2023-08-15 08:31:01 -0500

    fix: Landing Page title

--- desarrollo/src/app/modules/landing-page/pages/landing-page/landing-page.component.ts
+++ desarrollo/src/app/modules/landing-page/pages/landing-page/landing-page.component.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.less']
 })
-export class LandingPageComponent {
+export class LandingPageComponent implements OnInit{
+
+  constructor(@Inject(DOCUMENT) private document: Document, private translate: TranslateService) {
+  }
+
+  ngOnInit(): void {
+    this.translate.stream('LANDING_PAGE.TITLE').subscribe(
+      (res: string) => this.document.title = res
+    );
+    this.translate.stream('HEADER.AMT').subscribe(
+      (res: string) => this.document.title += ' - ' + res
+    )
+  }
 }
